329d9b239f340047043cb74a2a6ee0e553f33a9a
TheCountryGamer <dustinyost@Dustins-MacBook-Pro.local>
2013-09-04 21:02:57 -0400

    Added Possession class
    Added the EnumAbilities class for sorting mob abilites for later
    Added Possession class for some basic functions and sorting whe order
    in which functions must be called, and what they are and do

---
+++ Server/EnumAbilities.java
@@ -0,0 +1,7 @@
+package mods.CountryGamer_Possession.Server;
+
+public enum EnumAbilities {
+	
+	EXPLODE, BOWARROW, HIT, TELEPORT, THROW, SPRINT
+	
+}
---
+++ Server/Possession.java
@@ -0,0 +1,49 @@
+package mods.CountryGamer_Possession.Server;
+
+import java.util.ArrayList;
+
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.EnumCreatureAttribute;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.util.ResourceLocation;
+
+public class Possession {
+	
+	/*
+	 * Order of calling:
+	 * - get target stats
+	 * - set new EntityPossess to model and skin of target
+	 * -- getTargetModel & getTargetSkin
+	 * -- setModel and setSkin
+	 * - kill target
+	 * send player to EntityPossess / spawn EntityPossess
+	 * 
+	 */
+	
+	public static Object[] getTargetStats(EntityLivingBase targetEnt) {
+		float currentHealth = targetEnt.func_110143_aJ();
+		float maxHealth = targetEnt.func_110138_aP();
+		double attackDamage = 0;
+		// Assuming that this should get the generic damage, which is
+		// overridden on a per entity basis
+		// I believe 'func_111125_b' to be able to get the value required 
+		attackDamage = targetEnt.func_110140_aT().func_111150_b(
+				SharedMonsterAttributes.field_111264_e).func_111125_b();
+		ArrayList<EnumAbilities> abilities = null;
+		EnumCreatureAttribute creatureAttr = targetEnt.getCreatureAttribute();
+		
+		Object[] statArray = new Object[] {
+				maxHealth, attackDamage, abilities, creatureAttr
+			};
+		return statArray;
+	}
+	public static ModelBase getTargetModel(EntityLivingBase targetEnt) {
+		return null;
+	}
+	public static ResourceLocation getTargetSkin(EntityLivingBase taretEnt) {
+		return null;
+	}
+	
+}
