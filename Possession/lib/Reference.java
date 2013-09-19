e377974c5b12b84f2289d684dd5db8aa153a18fe
TheCountryGamer <dustinyost@Dustins-MacBook-Pro.local>
2013-09-19 10:31:33 -0400

    Fixed Servery Proxy

--- Possession/lib/Reference.java
+++ Possession/lib/Reference.java
@@ -9,7 +9,7 @@ public class Reference {
 	public static final String VERSION = "0.031"; //Need to decide on a standard for this
 	public static final String MC_VERSION = "1.6.2";
 	public static final String CHANNEL_NAME = MOD_ID;
-	public static final String SERVER_PROXY_CLASS = "CountryGamer_Possession.Possession.proxy.CommonProxy";
+	public static final String SERVER_PROXY_CLASS = "CountryGamer_Possession.Possession.proxy.ServerProxy";
 	public static final String CLIENT_PROXY_CLASS = "CountryGamer_Possession.Possession.proxy.ClientProxy";
 	
 }
